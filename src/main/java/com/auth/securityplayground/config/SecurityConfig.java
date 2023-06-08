422401075def09dd63cfec667db92f60c83a91e5
Abdurrahman Pektas <apektas@yandex.com>
2023-06-08 17:52:16 +0300

    - adding in memory users with no password encoder option

--- src/main/java/com/auth/securityplayground/config/SecurityConfig.java
+++ src/main/java/com/auth/securityplayground/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.auth.securityplayground.config;
 
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -16,12 +19,40 @@ public class SecurityConfig {
     // default config comes from configuration
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                .requestMatchers("/notices", "/contact").permitAll()
+        );
 
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
+
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails adminUser = User.builder()
+                .password("admin")
+                .username("admin")
+                .authorities("admin")
+                .build();
+
+        UserDetails regularUser = User.withUsername("user")
+                .password("user")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(adminUser, regularUser);
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
 
 
 }
