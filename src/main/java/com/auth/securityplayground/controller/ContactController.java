dbd18000887626e58360392a619a6e759d8a13c0
Abdurrahman Pektas <apektas@yandex.com>
2023-06-20 23:29:49 +0300

    - PreAuthorize and PostAuthorize usage

--- src/main/java/com/auth/securityplayground/controller/ContactController.java
+++ src/main/java/com/auth/securityplayground/controller/ContactController.java
@@ -4,10 +4,14 @@ package com.auth.securityplayground.controller;
 import com.auth.securityplayground.models.Contact;
 import com.auth.securityplayground.repository.ContactRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 @RestController
@@ -21,10 +25,18 @@ public class ContactController {
     }
 
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
+    //@PreFilter("!filterObject.contactName.equals('Test')") // remove contact is contactName == Test
+    @PostFilter("!filterObject.contactName.equals('Test')") // remove contact is contactName == Test
+    // prefilter and postfilter  works on collection type parameter
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
+        Contact contact = contacts.get(0);
         contact.setContactId(getServiceReqNumber());
         contact.setCreateDt(Instant.now());
-        return contactRepository.save(contact);
+        Contact savedContact = contactRepository.save(contact);
+
+        List<Contact> contactList = new ArrayList<>();
+        contactList.add(savedContact);
+        return contactList;
     }
 
     public String getServiceReqNumber() {
